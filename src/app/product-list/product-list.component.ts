ee5345b4b0d3b7864e81376d5588222d619b4e09
phap10122000 <trinhhaphap@gmail.com>
2020-03-23 14:09:08 +0700

    3 files modified, 2 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { from } from 'rxjs';
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
+  selected: Product;
+  constructor(private productService: ProductService) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  showDetail(product) {
+    this.selected = product;
+  }
+  getProducts() {
+    this.products = this.productService.listProducts();
+  }
+
+  // changeStatus(){
+  //   // this.product.status = !this.product.status;
+  //   // this.isDisplay = ! this.isDisplay;
+  // }
+  // changeName(e){
+  //   // this.product.name = e.target.value;
+  //   console.log(e.target.value);
+  // }
+
+
+  // showDetail(product){
+  //   this.selected = product;
+  //   // console.log(this.selected);
+  // }
+  // removeitem(id){
+  // //  console.log(id);
+  // //  this.products.filter(function(product){
+  // //    return this.product.id !== id;
+  // //  })
+  // this.products = this.products.filter(product=> product.id !== id);
+  // }
 
 }
--- src/app/product-manager/product-manager.component.ts
+++ src/app/product-manager/product-manager.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { from } from 'rxjs';
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-product-manager',
   templateUrl: './product-manager.component.html',
   styleUrls: ['./product-manager.component.css']
 })
 export class ProductManagerComponent implements OnInit {
+  products : Product[];
+  constructor(private productService: ProductService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getProducts();
   }
-
+  selected:Product;
+  showDetail(product) {
+    this.selected = product;
+  }
+  getProducts() {
+    this.products = this.productService.listProducts();
+  }
+  // showDetail(product){
+  //   this.selected = product;
+  //   console.log(this.selected);
+  // }
+  
+  removeitem(id){
+    //  console.log(id);
+    //  this.products.filter(function(product){
+    //    return this.product.id !== id;
+    //  })
+    this.products = this.products.filter(product=> product.id !== id);
+    }
 }
